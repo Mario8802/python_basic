748dcfb93f6b6143cdad43c41164413d2c561a18
Mario <144950133+Mario8802@users.noreply.github.com>
2023-10-14 22:17:22 +0200

    Add files via upload

---
+++ nested_loops/building.py
@@ -0,0 +1,5 @@
+floors = int(input())
+rooms = int(input())
+
+for floor in range(reversed(floors)):
+    print(floor)
---
+++ nested_loops/sum_prime_and_non_prime_numbers.py
@@ -0,0 +1,12 @@
+from math import sqrt
+number = int(input())
+flag = 0
+if number > 1:
+    for k in range(2,int(sqrt(number + 1))):
+        if number % k ==0:
+            flag = 1
+            break
+        if flag == 0:
+            print(f"{number} is a prime number")
+        else:
+            print(f"{number} is Not a Prime number")
